950706e3e878ecf00043363cceaa3073f07d1479
Shenole Latimer <shenole@gmail.com>
2019-02-16 11:05:42 -0500

    This is the first commit. app is functional, except for bug with being able to get back into the user dashboard by clicking the url back link after logging out

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="jumbotron text-center">
+        <h1>Welcome to Shenole Test Blog</h1>
+        <p>This is my first attempt at building a blog using Laravel.</p>
+        <p><a href="/login" class="btn btn-primary btn-lg mr-1" role="button">Login</a><a href="/register" class="btn btn-success btn-lg ml-1" role="button">Register</a></p>
+    </div>
+@endsection
